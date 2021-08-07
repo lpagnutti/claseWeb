2b6bcb8c48947e127f46697a93b8170fd099cd84
lpagnutti <lpagnutti@gmail.com>
2021-08-06 22:15:06 -0300

    redireccionando el index

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 app.use(express.static(path.join(__dirname, "./public")));
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/view/home.html"));
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 app.listen(3000, () => {
